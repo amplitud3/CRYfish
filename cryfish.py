6a8321de8b56570f67d7869256f3c45319028676
amplitud3 <111232829+amplitud3@users.noreply.github.com>
2026-04-06 15:11:06 +0530

    GameState class created

--- cryfish.py
+++ cryfish.py
@@ -3,7 +3,7 @@
 # board reprsentation
 # lower case for black
 # upper case for white 
-
+"""
 board = [ 
             [ 'r','n','b','q','k','b','n','r' ],  # blacks backrank
             [ 'p','p','p','p','p','p','p','p' ],
@@ -33,5 +33,60 @@ def print_board(board):
         print(k,end=" ")
     print()    
             
-print_board(board)          
+print_board(board)         
+"""
+
+
+
+class GameState:
+    def __init__(self):
+        self.board = [ 
+        
+            [ 'r','n','b','q','k','b','n','r' ],  # blacks backrank
+            [ 'p','p','p','p','p','p','p','p' ],
+            [ '.','.','.','.','.','.','.','.' ],
+            [ '.','.','.','.','.','.','.','.' ], 
+            [ '.','.','.','.','.','.','.','.' ], 
+            [ '.','.','.','.','.','.','.','.' ], 
+            [ 'P','P','P','P','P','P','P','P' ],
+            [ 'R','N','B','Q','K','B','N','R' ] # whites backrank
+            
+         ]
+        self.white_to_move = True # True-> white's turn False-> balck's turn
+        self.move_log=[ ] # to record every  move
+        
+        
+    def print_board(self):
+        for i in range(8):
+            rank = 8-i 
+            print(rank, end=" ")
+            for k in range(8):
+                print(self.board[i][k],end=" ")
+            print()    
+         
+        print("  " ,end="") 
+        for  k in [ 'a','b','c','d','e','f','g','h']:
+            print(k,end=" ")
+        print()    
+               
+gs=GameState()
+gs.print_board()        
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
            
